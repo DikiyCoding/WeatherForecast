43caa730925e3f90a995ca52834d53682273ae48
DikiyCoding <40475395+DikiyCoding@users.noreply.github.com>
2018-12-17 20:23:47 +0300

    part 3 (localization)

--- app/src/main/java/example/homework10/application/App.java
+++ app/src/main/java/example/homework10/application/App.java
@@ -2,18 +2,23 @@ package example.homework10.application;
 
 import android.app.Application;
 import android.arch.persistence.room.Room;
+
+import java.util.Locale;
+
 import example.homework10.database.AppDatabase;
 
 public class App extends Application {
 
     public static App instance;
     private AppDatabase database;
+    private Locale locale;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
         database = Room.databaseBuilder(this, AppDatabase.class, "database").build();
+        locale = getApplicationContext().getResources().getConfiguration().locale;
     }
 
     public static App getInstance() {
@@ -22,5 +27,9 @@ public class App extends Application {
 
     public AppDatabase getDatabase() {
         return database;
+    }
+
+    public Locale getLocale() {
+        return locale;
     }
 }
--- app/src/main/java/example/homework10/handlers/StringFormatHandler.java
+++ app/src/main/java/example/homework10/handlers/StringFormatHandler.java
@@ -3,9 +3,22 @@ package example.homework10.handlers;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import example.homework10.application.App;
+
 public class StringFormatHandler {
 
-    private static String directions[] = {"north", "northeast", "east", "southeast", "south", "southwest", "west", "northwest"};
+    private static String directions[];
+
+    static {
+        switch (App.getInstance().getLocale().toString()) {
+            case "en_US":
+                directions = new String[]{"north", "northeast", "east", "southeast", "south", "southwest", "west", "northwest"};
+                break;
+            case "ru_RU":
+                directions = new String[]{"северный", "северо-восточный", "восточный", "южно-восточный", "южный", "южно-западный", "западный", "северо-западный"};
+                break;
+        }
+    }
 
     public static String fromUNIXtoReadable(int unixSeconds) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm a", Locale.ENGLISH);       // выбераем, какой формат нужен
